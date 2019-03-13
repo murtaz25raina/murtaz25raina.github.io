8946f14280154dbbe407d81cf8e9460789cae3ba
murtaz <murtazraina25@gmail.com>
2019-03-13 14:31:18 +0530

    games

--- js/post.js
+++ js/post.js
@@ -27,29 +27,46 @@ var once=1;
 function editor(eds)
 {
     var vs = document.getElementById("inaminadica");
+    
+    var ds = document.getElementById("blogTitle");
+    
     if(once===1){
     var fun = document.getElementById("blogBody").innerHTML;
+    var fun1 = document.getElementById("blogTitleNew").innerHTML;
+
     document.getElementById("blogBody").setAttribute('style','display:none;');
+    document.getElementById("blogTitleNew").setAttribute('style','display:none;');    
     var par = document.createElement('textarea');
+    var par1 = document.createElement('textarea');
     var lis = document.createTextNode(fun.trim());
+    var lis1 = document.createTextNode(fun1.trim());
     par.appendChild(lis);
+    par1.appendChild(lis1);
     vs.appendChild(par);
+    ds.appendChild(par1);
     par.classList.add("editcmnt1");
+    par1.classList.add("editcmnt2");
   par.style.height = (par.scrollHeight)+"px";
+      par1.style.height = (par1.scrollHeight-25)+"px";
         document.getElementById("editing").innerHTML ='Save&nbsp;<i class="fas fa-save"></i>'; 
     once=2;
         
         document.getElementsByClassName("blogAuthor")[0].style.marginBottom="10px";
     }
     else {
-       var gun = document.getElementsByClassName("editcmnt1")[0].value; 
+       var gun = document.getElementsByClassName("editcmnt1")[0].value;
+        var gun1 = document.getElementsByClassName("editcmnt2")[0].value;
        console.log(gun);
         document.getElementById("blogBody").innerHTML =gun;
+        document.getElementById("blogTitleNew").innerHTML =gun1;
         document.getElementsByClassName("editcmnt1")[0].setAttribute('style','display:none;');
+        document.getElementsByClassName("editcmnt2")[0].setAttribute('style','display:none;');
         document.getElementById("blogBody").setAttribute('style','display:block;');
+        document.getElementById("blogTitleNew").setAttribute('style','display:block;');
          document.getElementById("editing").innerHTML ='Edit&nbsp;<i class="fas fa-edit"></i>';
         once=1;
         vs.removeChild(vs.lastChild);
+        ds.removeChild(ds.lastChild);
     }
         
 }
